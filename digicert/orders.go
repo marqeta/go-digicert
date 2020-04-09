1235e11422eed46649ec114ad6c064dd1a503a01
Blake Hitchcock <bhitchcock@marqeta.com>
2020-04-09 13:19:37 -0500

    Add a basic package for prototyping API access
    This commit introduces basic functionality for interacting with the
    DigiCert API.

---
+++ digicert/orders.go
@@ -0,0 +1,38 @@
+package digicert
+
+type OrdersService service
+
+type Order struct {
+	ID                          *int64       `json:"id"`
+	Certificate                 *Certificate `json:"certificate,omitempty"`
+	ValidityYears               *int         `json:"validity_years"`
+	CustomExpirationDate        *string      `json:"custom_expiration_date,omitempty"`
+	Comments                    *string      `json:"comments,omitempty"`
+	DisableRenewalNotifications *bool        `json:"disable_renewal_notifications,omitempty"`
+	RenewalOfOrderID            *int         `json:"renewal_of_order_id,omitempty"`
+	PaymentMethod               *string      `json:"payment_method,omitempty"`
+	Status                      *string
+}
+
+type orderList struct {
+	Orders *[]Order
+}
+
+func (o Order) String() string {
+	return Stringify(o)
+}
+
+func (s *OrdersService) List() (*[]Order, *Response, error) {
+	req, err := s.client.NewRequest("GET", "order/certificate", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	list := new(orderList)
+	resp, err := s.client.Do(req, list)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return list.Orders, resp, nil
+}
---
+++ digicert/strings.go
@@ -0,0 +1,89 @@
+package digicert
+
+// NOTE: As of this time, this file is unmodified from the go-github project
+// version.
+
+// Copyright 2013 The go-github AUTHORS. All rights reserved.
+// https://github.com/google/go-github/blob/4a7cf4a76843a3e11ce2bfd96a9b5067a07a2429/github/strings.go
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"reflect"
+)
+
+var timestampType = reflect.TypeOf(Timestamp{})
+
+func Stringify(message interface{}) string {
+	var buf bytes.Buffer
+	v := reflect.ValueOf(message)
+	stringifyValue(&buf, v)
+	return buf.String()
+}
+
+func stringifyValue(w io.Writer, val reflect.Value) {
+	if val.Kind() == reflect.Ptr && val.IsNil() {
+		w.Write([]byte("<nil>"))
+		return
+	}
+
+	v := reflect.Indirect(val)
+
+	switch v.Kind() {
+	case reflect.String:
+		fmt.Fprintf(w, `"%s"`, v)
+	case reflect.Slice:
+		w.Write([]byte{'['})
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				w.Write([]byte{' '})
+			}
+
+			stringifyValue(w, v.Index(i))
+		}
+
+		w.Write([]byte{']'})
+		return
+	case reflect.Struct:
+		if v.Type().Name() != "" {
+			w.Write([]byte(v.Type().String()))
+		}
+
+		// special handling of Timestamp values
+		if v.Type() == timestampType {
+			fmt.Fprintf(w, "{%s}", v.Interface())
+			return
+		}
+
+		w.Write([]byte{'{'})
+
+		var sep bool
+		for i := 0; i < v.NumField(); i++ {
+			fv := v.Field(i)
+			if fv.Kind() == reflect.Ptr && fv.IsNil() {
+				continue
+			}
+			if fv.Kind() == reflect.Slice && fv.IsNil() {
+				continue
+			}
+
+			if sep {
+				w.Write([]byte(", "))
+			} else {
+				sep = true
+			}
+
+			w.Write([]byte(v.Type().Field(i).Name))
+			w.Write([]byte{':'})
+			stringifyValue(w, fv)
+		}
+
+		w.Write([]byte{'}'})
+	default:
+		if v.CanInterface() {
+			fmt.Fprint(w, v.Interface())
+		}
+	}
+}
